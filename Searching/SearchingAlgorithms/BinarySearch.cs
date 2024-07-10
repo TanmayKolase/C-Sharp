61deae9662554ec3ff053fc17947293974d6d6aa
TanmayKolase <tanmaykolase07@gmail.com>
2024-07-10 17:46:56 +0530

    Programms regarding searching

---
+++ Searching/SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,48 @@
+﻿namespace SearchingAlgorithms
+{
+    public class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int size = arr.Length;
+
+            // User enters the values
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter " + (i + 1) + " element: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter the element to search: ");
+            int target = int.Parse(Console.ReadLine());
+
+            int result = BinarySearchAlgorithm(arr, size, target);
+
+            if (result != -1)
+                Console.WriteLine("Element is present at index " + result);
+            else
+                Console.WriteLine("Element is not present in array");
+        }
+
+        public static int BinarySearchAlgorithm(int[] arr, int size, int target)
+        {
+            int left = 0;
+            int right = size - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == target)
+                    return mid;
+
+                if (arr[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Searching/SearchingAlgorithms/LinearSearch.cs
@@ -0,0 +1,40 @@
+﻿namespace SearchingAlgorithms
+{
+    public class LinearSearch
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int size = arr.Length;
+
+            // User enters the values
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter " + (i + 1) + " element: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter the element to search: ");
+            int target = int.Parse(Console.ReadLine());
+
+            int result = LinearSearchAlgorithm(arr, size, target);
+
+            if (result != -1)
+                Console.WriteLine("Element found at index: " + result);
+            else
+                Console.WriteLine("Element not found!");
+        }
+
+        public static int LinearSearchAlgorithm(int[] arr, int size, int target)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
